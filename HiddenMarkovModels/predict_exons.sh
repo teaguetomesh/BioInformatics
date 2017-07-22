23a4a2416c6a5145f440db71d884834649913583
Teague Tomesh <tomeshteague@gmail.com>
2017-07-22 16:54:28 -0400

    Add files via upload

---
+++ HiddenMarkovModels/predict_exons.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Uncomment the line below to run a compiled C or C++ program
+#./predict_exons $1 $2
+
+# Uncomment the line below if you write a Java program
+#java predict_exons $1 $2
+
+# Uncomment the line below for a Python program
+python predict_exons.py $1 $2
+
+# Uncomment the line below for a Perl program
+#perl predict_exons.pl $1 $2
+
+# Uncomment the line below for an R program
+#Rscript predict_exons.R $1 $2
